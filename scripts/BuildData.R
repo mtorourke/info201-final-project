e60fb57aadb4b2581cdcf860bbbb2b5580c383e3
Evan Van Cotthem <evancotthme@gmail.com>
2016-12-03 18:48:59 -0800

    Fixed bug in score calculation

--- scripts/BuildData.R
+++ scripts/BuildData.R
@@ -13,17 +13,21 @@ data <- read.csv("team-picking-categories.csv")
 #  select(-(SLP: NYP)) %>% 
 #  mutate(total.score = 0)
 
-CalculateScores <- function(data, multipliers) {
+#Calculates the score of each team based on the weighting of each category
+#Returns the data frame with the teams sorted by their score (highest = first)
+CalculateScores <- function(data, cat.weights) {
   for(i in 1:32) {
-    data[i, "total.score"] <- TeamScore(i, multipliers)
+    data[i, "total.score"] <- TeamScore(data, i, cat.weigths)
   }
+  data <- arrange(data, -total.score)
   return(data)
 }
 
-TeamScore <- function(team, multipliers) {
+#Calculates and returns the score of a specific team based on the weighting of each category
+TeamScore <- function(data, team, cat.weigths) {
   score <- 0
   for(i in 1:14) {
-    score <- score + (data[team, i+1] * multipliers[i])
+    score <- score + (data[team, i+1] * cat.weights[i])
   }
   return(score)
 }
