14de10c392d28c2a0f9b0823ebf00423d6677d4a
avania <avani.amin96@gmail.com>
2016-12-06 19:13:13 -0800

    New visualization script

---
+++ scripts/TeamVisualization.R
@@ -0,0 +1,15 @@
+#Library programs
+library(plotly)
+library(stringr)
+
+# Creates function to search for a team
+TeamChart <- function(data,  search = '') {
+  data <- data %>% 
+    filter(grepl(search, team))
+  data <- as.data.frame(t(data))
+  
+  column <- paste0("data$", search)
+  y <- eval(parse(text = column))
+  
+  plot_ly(data = data, x = ~Team, y= y)
+}
